4f41743427769a1fc9fae8a984e546ec8921c475
santi4246 <santiago_pna@hotmail.com>
2025-03-13 19:38:32 -0300

    feat/Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,70 @@
 import './App.css';
-import { Menu, type MenuProps } from 'antd';
-import { MailOutlined } from '@ant-design/icons';
+import { Menu, MenuTheme, Switch, type MenuProps } from 'antd';
+import { MenuOutlined, WhatsAppOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
+const showSideBar = () => {
+  console.log("Show Menu");
+  /* const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex'; */
+}
+
 const items: MenuItem[] = [
   {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
+    label: `Municipio de ${process.env.REACT_APP_NOMBRE_MUNICIPIO}`,
+    key: 'logo',
+    style: { marginRight: 'auto', opacity: '0.5', pointerEvents: 'none' },
+  },
+  {
+    label: 'Infracciones',
+    key: 'infracciones',    
+  },
+  {
+    label: 'Contacto',
+    key: 'contacto',
+    icon: <WhatsAppOutlined />,
+  },
+  {
+    label: '',
+    key: 'menu',
+    icon: <MenuOutlined />,
+    style: { opacity: '1', paddingRight: '5px' },    
+    onClick: () => showSideBar
   },
 ];
 
 function App() {
-  const [current, setCurrent] = useState('mail');
+  const [current, setCurrent] = useState('');
+  const [theme, setTheme] = useState<MenuTheme>('dark');
+  const [hovered, setHovered] = useState(false);
+  const handleMouseEnter = () => setHovered(true);
+  const handleMouseLeave = () => setHovered(false);
 
   const onClick: MenuProps['onClick'] = (e) => {
     console.log('click ', e);
     setCurrent(e.key);
   };
 
+  const changeTheme = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+  };
+
   return (
-    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+    <div>
+      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} style = {{}} theme = { theme }/>
+      <Menu onClick={onClick} selectedKeys={[current]} mode="vertical" items={items} className = 'sidebar' theme = { theme }/>
+      <div style = {{ margin: '15px 15px' }}>
+      <Switch
+        checked={theme === 'dark'}
+        onChange={changeTheme}
+        checkedChildren="Dark"
+        unCheckedChildren="Light"
+        style = {{ display: 'flex', marginLeft: 'auto' }} 
+      />
+      </div>
+    </div>    
   )
 }
 
