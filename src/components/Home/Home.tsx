55622c2f6493d7d555e110bc686124e852521458
Santiago L. Romero <santiago_pna@hotmail.com>
2025-03-17 12:50:59 -0300

    feat/FetchData

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,10 @@
 import type { FormProps } from 'antd';
 import { Button, Form, Input, MenuTheme, Space, } from 'antd';
+import '@ant-design/v5-patch-for-react-19';
 import { Content } from 'antd/es/layout/layout';
 import "../../App.css";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import useInfraccionApi, { ActionResult, Infraccion } from '../../hooks/useInfraccionApi';
 
 
 interface HomeProps {
@@ -14,23 +16,34 @@ type FieldType = {
     CuitCuil?: string;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-    console.log('Success:', values.CuitCuil);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-};
-
 const Home = ({ theme, setTheme }: HomeProps) => {
+    const api = useInfraccionApi();
+    const [ infracciones, setInfracciones ] = useState<Infraccion[]>([]);
     const labelColor = theme === 'dark' ? '#ccc' : '#000';
-    const inputTextColor = theme === 'dark' ? '#000' : '#000';  // Mantener el texto negro para ambos modos
-    const inputBgColor = theme === 'dark' ? '#ffffff' : '#ffffff';  // Fondo blanco para los inputs en ambos modos
-    const inputBorderColor = theme === 'dark' ? '#555555' : '#d9d9d9'; // Ajustar borde según el tema
+    const inputTextColor = theme === 'dark' ? '#000' : '#000';
+    const inputBgColor = theme === 'dark' ? '#ffffff' : '#ffffff';
+    const inputBorderColor = theme === 'dark' ? '#555555' : '#d9d9d9';
 
     useEffect(() => {
-        console.log("Tema: ", theme);
+        api.getToken().then(response => {
+            localStorage.setItem("_token", response.data!.value.token);            
+        }).catch(error => console.log(`Error: ${error}`));
     }, []);
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        console.log('Success:', values.CuitCuil);
+        const response = await api.list(values.CuitCuil!, localStorage.getItem("_token")!);
+        setInfracciones(response.data!.value);
+    };
+    
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    useEffect(() => {
+        console.log("Infracciones: ", infracciones);
+    }, [infracciones]);
+
     return (
         <Content className='content' style={{ backgroundColor: theme === 'dark' ? 'var(--primary-color)' : 'var(--secondary-color)' }}>
             <Space className='space'>
@@ -56,12 +69,12 @@ const Home = ({ theme, setTheme }: HomeProps) => {
                         style={{ color: labelColor, wordWrap: 'break-word', lineHeight: '1.5', width: '100%', fontSize: '0.8rem' }}
                         className='ant-form-item-label'
                     >
-                        <Input 
-                        style={{
-                            color: inputTextColor,
-                            backgroundColor: inputBgColor,
-                            borderColor: inputBorderColor
-                          }}
+                        <Input
+                            style={{
+                                color: inputTextColor,
+                                backgroundColor: inputBgColor,
+                                borderColor: inputBorderColor
+                            }}
                         />
                     </Form.Item>
                     <Form.Item label={null} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
